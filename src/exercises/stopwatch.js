2d688b645969bf611f404646ccdb08db4b0ccecd
Zain Fathoni <zain.fathoni@gmail.com>
2018-12-12 02:36:56 +0800

    07 Interact and Update State in React with useState

--- src/exercises/stopwatch.js
+++ src/exercises/stopwatch.js
@@ -1,18 +1,44 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 function Stopwatch() {
+  const [lapse, setLapse] = useState(0)
+  const [running, setRunning] = useState(false)
+  const intervalRef = useRef(null)
+
+  // Make sure that the callback function is only run one time and that it returns the cleanup function
+  useEffect(() => () => clearInterval(intervalRef.current), [])
+
+  function handleRunClick() {
+    if (running) {
+      clearInterval(intervalRef.current)
+    } else {
+      const startTime = Date.now() - lapse
+      intervalRef.current = setInterval(() => {
+        setLapse(Date.now() - startTime)
+      }, 0)
+    }
+    setRunning(!running)
+  }
+
+  function handleClearClick() {
+    clearInterval(intervalRef.current)
+    setLapse(0)
+    setRunning(false)
+  }
+
   return (
     <div style={{textAlign: 'center'}}>
       <label
         style={{
+          fontFamily: 'monospace',
           fontSize: '5em',
           display: 'block'
         }}
       >
-        0ms
+        {lapse}ms
       </label>
-      <button style={buttonStyles}>Start</button>
-      <button style={buttonStyles}>Clear</button>
+      <button onClick={handleRunClick} style={buttonStyles}>{running ? 'Stop' : 'Start'}</button>
+      <button onClick={handleClearClick} style={buttonStyles}>Clear</button>
     </div>
   )
 }
