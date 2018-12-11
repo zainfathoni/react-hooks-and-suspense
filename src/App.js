2c3a166b9e8692b4b38e3f16467da1fc264f1b2c
Zain Fathoni <zain.fathoni@gmail.com>
2018-12-12 03:11:53 +0800

    Setup 11

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 import './App.css'
-import Stopwatch from './exercises/stopwatch'
+import Name from './exercises/name'
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <div className="stopwatch">
-          <Stopwatch />
+        <div className="name">
+          <Name />
         </div>
       </div>
     )
---
+++ src/exercises/name.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+function Upper({ children }) {
+  const [count, setCount] = useState(0)
+  console.log('rendering', children)
+  return (
+    <div>
+      Uppercase version: {children.toUpperCase()}{' '}
+      <button onClick={() => setCount(count + 1)}>{count}</button>
+    </div>
+  )
+}
+
+function Name() {
+  const [first, setFirstName] = useState('')
+  const [last, setLastName] = useState('')
+  return (
+    <div>
+      <label htmlFor="first-name-input">First Name</label>
+      <input id="first-name-input" onChange={e => setFirstName(e.target.value)} type="text" />
+      <Upper>{first}</Upper>
+      <hr />
+      <label htmlFor="last-name-input">Last Name</label>
+      <input id="last-name-input" onChange={e => setLastName(e.target.value)} type="text" />
+      <Upper>{last}</Upper>
+    </div>
+  );
+}
+
+export default Name
