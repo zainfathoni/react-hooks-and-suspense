d2d7690e33196ba8ca9986850f06828029f1b308
Zain Fathoni <zain.fathoni@gmail.com>
2018-12-12 01:51:18 +0800

    05 Test Effects Generated from React the useEffect Hook

--- src/exercises/counter.test.js
+++ src/exercises/counter.test.js
@@ -1,6 +1,11 @@
 import React from 'react'
-import { render, fireEvent } from 'react-testing-library'
+import { cleanup, fireEvent, render } from 'react-testing-library'
 import Counter from './counter'
+
+afterEach(() => {
+  window.localStorage.removeItem('count')
+  cleanup()
+})
 
 test('counter increments the count', () => {
   const { container } = render(<Counter />)
@@ -8,4 +13,15 @@ test('counter increments the count', () => {
   expect(button.textContent).toBe('0')
   fireEvent.click(button)
   expect(button.textContent).toBe('1')
+})
+
+test('reads and updates localStorage', () => {
+  window.localStorage.setItem('count', 3)
+  const { container, rerender } = render(<Counter />)
+  const button = container.firstChild
+  expect(button.textContent).toBe('3')
+  fireEvent.click(button)
+  expect(button.textContent).toBe('4')
+  rerender(<Counter />)
+  expect(window.localStorage.getItem('count')).toBe('4')
 })
